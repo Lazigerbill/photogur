3f266f135dd3a2f026544bc30922f5fafd24007b
Lazigerbill <chaukbill@gmail.com>
2014-08-26 14:50:09 -0400

    3 sample pics are added

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,19 @@
 class PicturesController < ApplicationController
 	def index
+		@pictures = {
+			:title=>"The walking dead",
+			:author=>"Rick Grimes",
+			:url=>"http://image-cdn.zap2it.com/photogallery/images/zap-the-walking-dead-season-4-pics-039"
+	},
+			{
+			:title=>"Breaking Bad",
+			:author=>"Walter White",
+			:url=>"http://cdn.screenrant.com/wp-content/uploads/breaking-bad-season-5-king.jpg"
+	},
+			{
+			:title=>"Mad Men",
+			:author=>"Don Drapper",
+			:url=>"http://cdn.sheknows.com/articles/2013/05/mad-men-s6-e7-don.jpg"
+	}		
 	end
 end
