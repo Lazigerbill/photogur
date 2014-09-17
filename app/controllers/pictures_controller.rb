b52d5d06945dd0b14b3b046a898d76ddba99de4a
Lazigerbill <chaukbill@gmail.com>
2014-09-17 18:10:48 -0400

    added search box and create function in index page

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,23 +1,45 @@
 class PicturesController < ApplicationController
 	def index
-		@pictures = Picture.all
+		@picture = Picture.new
+		@pictures = if params[:search]
+			Picture.where("LOWER(character) LIKE LOWER(?)", "%#{params[:search]}%")
+		else
+			Picture.all
+		end
 	end
+
 	def show
-		@pic = Picture.find(params[:id])
+		@picture = Picture.find(params[:id])
 	end
 
 	def new
-		@pic = Picture.new
+		@picture = Picture.new
 	end
 
 	def create
-		@pic = Picture.new(pic_params)
-		if @pic.save
+		@picture = Picture.new(pic_params)
+		if @picture.save
 			redirect_to pictures_url
 		else
-		render :new #i.e. new.html.erb
+		flash[:alert] = "Invalid input, please re-enter."
+		@pictures = Picture.all
+		render :index 
+
 		end
 	end
+
+	def update
+		@picture=Picture.find(params[:id])
+		@picture.update_attributes(pic_params)
+		redirect_to "/pictures/#{@picture.id}"
+	end
+
+	def destroy
+		@picture = Picture.find(params[:id])
+		@picture.destroy
+		redirect_to pictures_url
+	end
+
 	
 	private
 	def pic_params
